4d68a124bd76c5ba6dd0a057c9fcc247e060667d
praveen <238w1a1285@vrsec.ac.in>
2026-05-20 11:49:23 +0530

    Optimized embeddings model for deployment

--- rag/embeddings.py
+++ rag/embeddings.py
@@ -1,7 +1,18 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 
+embedding_model = None
+
+
 def get_embedding_model():
 
-    return HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
-    )
+    global embedding_model
+
+    if embedding_model is None:
+
+        print("Loading lightweight embedding model...")
+
+        embedding_model = HuggingFaceEmbeddings(
+            model_name="sentence-transformers/paraphrase-MiniLM-L3-v2"
+        )
+
+    return embedding_model
